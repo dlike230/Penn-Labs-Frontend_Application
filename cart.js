6b5abaf9dcb0b6b97958f4b620a6677f0c00c80e
dlike230 <dlike230@gmail.com>
2018-09-23 00:43:52 -0400

    Updates to cart

--- cart.js
+++ cart.js
@@ -13,6 +13,9 @@ var cart_mini_number_container = $("#cart_mini_number_container")[0];
 //node referring to cart container
 var cart_container = $("#cart_container")[0];
 
+//node referring to cart item list
+var cart_list = $("#cart_update_location")[0];
+
 //represents whether just the full cart, or just the mini cart indicator, is displayed
 var full_cart_displayed = false;
 
@@ -47,6 +50,19 @@ function display_cart(){
 	cart_mini_number_container.style.opacity = 1;
 }
 
+//returns a node for an item within the cart
+function generate_cart_item_node(){
+	var node = document.createElement("LI");
+	node.setAttribute("class","list-group-item");
+	var title = document.createElement("H4");
+	title.textContent = "Lenn 100";
+	var price = document.createElement("H5");
+	price.textContent = "$300";
+	node.appendChild(title);
+	node.appendChild(price);
+	return node;
+}
+
 function add_to_cart(){
 	if(!cart_displayed){
 		display_cart();
@@ -54,20 +70,22 @@ function add_to_cart(){
 	}
 	cart_size ++;
 	update_cart_size_number();
+	cart_list.appendChild(generate_cart_item_node());
 }
 
 function display_full_cart(){
 	full_cart_displayed = true;
 	cart_mini_number_container.style.visibility = "hidden";
-	cart_container.setAttribute("class",cart_container.getAttribute("class")+" triggered");
+	cart_container.setAttribute("class","triggered");
 }
 
 function collapse_cart(){
 	full_cart_displayed = false;
-	cart_container.setAttribute("class",cart_container.getAttribute("class").replace(" triggered",""));	
-	//waits until the closing animation is finished to re-display the quantity indicator
+	cart_container.setAttribute("class","");	
+	//waits until the closing animation is finished to re-display the quantity indicator and hide the cart
 	setTimeout(function(){
 		cart_mini_number_container.style.visibility = "visible";
+		cart_container.setAttribute("class","gone");		
 	},400);
 }
 
@@ -79,3 +97,15 @@ function toggle_cart_size(){
 		display_full_cart();
 	}
 }
+
+
+//collapses cart if clicked outside of
+$(window).click(function() {
+	if(full_cart_displayed){
+		collapse_cart();
+	}
+});
+
+$('#cart_container').click(function(event){
+    event.stopPropagation();
+});
